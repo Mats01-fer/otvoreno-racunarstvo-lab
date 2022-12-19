33c8d04bdf1fbb2df90d7ba18572af8f6ba2997c
Mats01 <matej.butkovic@gmail.com>
2022-12-19 16:54:03 +0100

    swagger

--- orf1/orf1/urls.py
+++ orf1/orf1/urls.py
@@ -22,6 +22,23 @@ from .settings import STATIC_URL
 
 
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 
 router = routers.DefaultRouter()
@@ -31,6 +48,11 @@ router.register(r'results', ResultViewSet)
 router2 = routers.DefaultRouter('api/v2')
 
 urlpatterns = [
+    
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+   
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     
